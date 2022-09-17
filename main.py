3cfe0e4ff01230658297746b8ef38b2987150bb9
Dislate <ice_balyk@mail.ru>
2022-09-17 14:13:25 +0300

    getting data, generating report and etc...

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import utils
+
+if __name__ == '__main__':
+    # Получение всех пользователей и задач
+    all_users, all_tasks = utils.get_users_and_tasks("https://json.medrocket.ru/users",
+                                                     "https://json.medrocket.ru/todos")
+    # Создание директории с отчетами
+    utils.create_dir_if_not_exist("tasks")
+    for user in all_users:
+        # Проверка папки пользователя с отчетами
+        user_path = f"tasks/{user['name']}"
+        utils.create_dir_if_not_exist(user_path)
+        # Список задач пользователя
+        # Оптимизация за счет удаления из списка уже отобранных задач, уменьшая тем самым количество итераций
+        user_tasks = [all_tasks.pop(i) for i in range(len(all_tasks))[::-1] if all_tasks[i]["userId"] == user["id"]]
+        completed_tasks = [task for task in user_tasks if task["completed"]]
+        exp_tasks = [task for task in user_tasks if not task["completed"]]
+
+        report = utils.generate_report(user, completed_tasks, exp_tasks)
+        utils.save_report(report, user_path, user["name"])
---
+++ utils.py
@@ -0,0 +1,75 @@
+import os
+import time
+import requests
+from datetime import datetime
+
+
+def get_users_and_tasks(url_users: str, url_tasks: str) -> tuple:
+    try:
+        all_users = requests.get(url_users).json()
+        all_tasks = [task for task in requests.get(url_tasks).json() if
+                     len(task) == 4]  # Последние три записи не валидны, так как имеют один ключ 'id'
+        return all_users, all_tasks
+    except requests.exceptions.ConnectionError:
+        print("Проверьте свое интернет соединение.\n"
+              "Повторная попытка получить данные произойдет через 5 секунд\n"
+              "-----------------------------------")
+        time.sleep(5)
+        return get_users_and_tasks(url_users, url_tasks)
+
+
+def create_dir_if_not_exist(path: str = "") -> None:
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+
+def generate_report(user: dict, comp_tasks: list[dict], exp_tasks: list[dict]) -> str:
+    # Получение даты на момент создания отчета
+    time_now = datetime.now()
+    # Шапка отчета
+    header_report = f'''Отчёт для {user["company"]["name"]}.
+{user["name"]} <{user["email"]}> {time_now.day}.{time_now.month}.{time_now.year} {time_now.hour}:{time_now.minute}
+Всего задач: {len(comp_tasks) + len(exp_tasks)}\n\n'''
+    # Завершенные задачи отчета
+    comp_report = f'Завершенные задачи ({len(comp_tasks)}):\n'
+    for comp_task in comp_tasks:
+        comp_report += name_task(comp_task)
+    comp_report += '\n'
+    # Оставшиеся задачи отчета
+    exp_report = f'Оставшиеся задачи ({len(exp_tasks)}):\n'
+    for exp_task in exp_tasks:
+        exp_report += name_task(exp_task)
+
+    return header_report + comp_report + exp_report
+
+
+def name_task(task: dict) -> str:
+    if len(task["title"]) > 48:
+        return f'{task["title"][:48]}...\n'
+    return f'{task["title"]}\n'
+
+
+def check_exist_report(report_path: str) -> bool:
+    if os.path.isfile(report_path):
+        return True
+    return False
+
+
+def save_report(report: str, user_path: str, user_name: str) -> None:
+    file_name = '_'.join(user_name.split(' '))
+    if check_exist_report(f'{user_path}/{file_name}.txt'):
+        rename_old_report(user_path, file_name)
+    with open(f"{user_path}/{file_name}.txt", "w") as f:
+        f.write(report)
+        f.close()
+
+
+def rename_old_report(user_path: str, file_name: str) -> None:
+    # Получение даты создания отчета
+    c_time_report = os.path.getctime(f'{user_path}/{file_name}.txt')
+    # Получение нормальной даты создания отчета
+    n_time = datetime.fromtimestamp(c_time_report)
+    # Генерация имени старого отчета
+    old_report_name = f'old_{file_name}_{n_time.year}-{n_time.month}-{n_time.day}T{n_time.hour}:{n_time.minute}.txt'
+    # Переименовывание старого отчета
+    os.rename(f"{user_path}/{file_name}.txt", f"{user_path}/{old_report_name}")
